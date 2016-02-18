6f0e9669f3982f9ee22e2b04442972b6ec9c4b33
Andrew Cruickshank <acruickshank42@gmail.com>
2016-02-18 19:36:17 +0000

    changed back to int

--- src/main/java/com/digitalbear/RomanNumerals.java
+++ src/main/java/com/digitalbear/RomanNumerals.java
@@ -9,7 +9,9 @@ public class RomanNumerals {
     static Map roman = new HashMap();
 
     public static String getRomanNumeral(int num) {
-        roman.put("1", "I");
-        return (String) roman.get("1");
+        roman.put(1, "I");
+        roman.put(4, "IV");
+        roman.put(5, "V");
+        return (String) roman.get(num);
     }
 }
