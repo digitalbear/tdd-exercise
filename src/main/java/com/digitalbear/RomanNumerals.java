92d6ba0a23d910ccb699f11e108b1c04e4327d86
Andrew Cruickshank <acruickshank42@gmail.com>
2016-02-19 23:03:58 +0000

    working

--- src/main/java/com/digitalbear/RomanNumerals.java
+++ src/main/java/com/digitalbear/RomanNumerals.java
@@ -1,29 +1,33 @@
 package com.digitalbear;
 
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 public class RomanNumerals {
-
 
     static int[] ints = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
     static String[] numerals = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+    String numeral = "";
 
+    public String getRomanNumeral(int inputNumber) {
+        while (inputNumber > 0){
+            inputNumber = getNext(inputNumber, findFloorIndex(inputNumber));
+        }
+        return numeral;
+    }
 
+    public int getNext(int num, int floorIndex) {
+        num = num - ints[floorIndex];
+        numeral = numeral + numerals[floorIndex];
+        return num;
+    }
 
-    public static String getRomanNumeral(int num) {
-        String retVal="";
-        while (num > 0){
-        for(int i = 0; i<ints.length ;i++){
+    public int findFloorIndex(int num){
+        int index = 0;
+        for(int i = 0; i < ints.length; i++) {
             if(num >= ints[i]){
-                num=num-ints[i];
-                retVal=retVal+ numerals[i];
+                index = i;
+                break;
             }
-        }}
-        return retVal;
-
+        }
+        return index;
     }
 }
--- src/test/java/com/digitalbear/RomanNumeralsTest.java
+++ src/test/java/com/digitalbear/RomanNumeralsTest.java
@@ -1,30 +1,50 @@
 package com.digitalbear;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralsTest {
 
+    RomanNumerals roman;
+
+    @Before
+    public void createInstance() {
+        roman = new RomanNumerals();
+    }
+
     @Test
     public void check_number_1() {
-        assertEquals("I", RomanNumerals.getRomanNumeral(1));
+        assertEquals("I", roman.getRomanNumeral(1));
     }
 
     @Test
     public void check_number_4() {
-        assertEquals("IV", RomanNumerals.getRomanNumeral(4));
+        assertEquals("IV", roman.getRomanNumeral(4));
     }
     @Test
     public void check_number_1000() {
-        assertEquals("M", RomanNumerals.getRomanNumeral(1000));
+        assertEquals("M", roman.getRomanNumeral(1000));
     }
     @Test
     public void check_number_2() {
-        assertEquals("II", RomanNumerals.getRomanNumeral(2));
+        assertEquals("II", roman.getRomanNumeral(2));
     }
     @Test
     public void check_number_36() {
-        assertEquals("XXXVI", RomanNumerals.getRomanNumeral(36));
+        assertEquals("XXXVI", roman.getRomanNumeral(36));
+    }
+    @Test
+    public void check_number_42() {
+        assertEquals("XLII", roman.getRomanNumeral(42));
+    }
+    @Test
+    public void check_number_87() {
+        assertEquals("LXXXVII", roman.getRomanNumeral(87));
+    }
+    @Test
+    public void check_number_249() {
+        assertEquals("CCXLIX", roman.getRomanNumeral(249));
     }
 }
